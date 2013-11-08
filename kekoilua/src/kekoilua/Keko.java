1969458f3d6de9be0a07067558dca20f903bc46d
Johanna <johanna.keto@helsinki.fi>
2013-11-08 14:27:55 +0200

    keko-rajapinta, jonka erityyppiset keot toteuttavat

---
+++ kekoilua/src/kekoilua/Keko.java
@@ -0,0 +1,13 @@
+
+package kekoilua;
+
+import java.util.ArrayList;
+
+
+public interface Keko {
+    public void BuidHeap();
+    public void Heapify(int i, ArrayList<Integer> keko);
+    public void Delete(int i, ArrayList<Integer> keko);
+    public void Insert(int i, ArrayList<Integer> keko);
+    
+}
