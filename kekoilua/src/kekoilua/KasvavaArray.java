1c460aa872bf0812d5923b833fea80478723a66f
Johanna <johanna.keto@helsinki.fi>
2013-11-16 00:40:44 +0200

    vähän korjailuja kasvavaan Arrayhyn

--- kekoilua/src/kekoilua/KasvavaArray.java
+++ kekoilua/src/kekoilua/KasvavaArray.java
@@ -5,23 +5,34 @@ import java.util.List;
 
 public class KasvavaArray<T> {
 
+    int viimeinen;
     T[] lista;
+    Class<T> cc;
 
     public KasvavaArray(Class<T> c) {
         @SuppressWarnings("unchecked")
-        final T[] a = (T[]) Array.newInstance(c, 100);
+        T[] a = (T[]) Array.newInstance(c, 100);
+        cc = c;
         lista = a;
     }
 
     public void add(T alkio) {
-        T[] kopio = new T[lista.length + 1];
-        kopio = lista.clone();
-        kopio[kopio.length] = alkio;
-        this.lista = kopio;
+        if (viimeinen == lista.length - 1) {
+            @SuppressWarnings("unchecked")
+            T[] kopio = (T[]) Array.newInstance(cc, lista.length + 1);
+            kopio = lista.clone();
+            kopio[kopio.length] = alkio;
+            this.lista = kopio;
+        } else {
+            lista[viimeinen + 1] = alkio;
+            viimeinen = viimeinen + 1;
+        }
+
     }
 
     public void delete(T alkio) {
-        T[] kopio = new T[lista.length-1];
+        @SuppressWarnings("unchecked")
+        T[] kopio = (T[]) Array.newInstance(cc, lista.length - 1);
         for (int j = 0; j <= lista.length; j++) {
             for (int i = 0; i < lista.length; i++) {
                 if (lista[i] == alkio) {
