91945d4bbae7edfea6f02137437e9a2671392d24
Johanna <johanna.keto@helsinki.fi>
2013-11-15 23:06:22 +0200

    binomit hetkeksi jäihin

--- kekoilua/src/kekoilua/Binomikeko.java
+++ kekoilua/src/kekoilua/Binomikeko.java
@@ -1,43 +1,71 @@
 package kekoilua;
 
+public class Binomikeko implements Keko {
 
-public class Binomikeko implements Keko{
-    
-    private void merge(int[]eka, int[]toka){
+    private Node juuri;
+    private Binomikeko(){
+        this.juuri = null;
+    }
+
+    private void Merge(Node toka) {
+         while(juuri != null && toka != null){
+             Node tree = MergeTree(juuri, toka);
+             if(juuri != null){
+                 tree = MergeTree(tree, );
+             }
+         }
         /*
          * while not (p.end() and q.end())
-        tree = mergeTree(p.currentTree(), q.currentTree())
+         tree = mergeTree(p.currentTree(), q.currentTree())
         
-        if not heap.currentTree().empty()
-            tree = mergeTree(tree, heap.currentTree())
+         if not heap.currentTree().empty()
+         tree = mergeTree(tree, heap.currentTree())
         
-        heap.addTree(tree)
-        heap.next(); p.next(); q.next()
+         heap.addTree(tree)
+         heap.next(); p.next(); q.next()
+         * 
+         * 
          */
+        
     }
     
+    private Node addSubTree(Node eka, Node toka){
+        eka.setNext(toka);
+        return eka;
+    }
+
+    private Node MergeTree(Node toka) {
+        if(eka.getValue() <= toka.getValue()){
+            return addSubTree(juuri, toka);
+        }else{
+            return addSubTree(toka, juuri);
+        }
+    }
+
     @Override
     public void Delete() {
+        
         /*
          * function deleteMin(heap)
-    min = heap.trees().first()
-    for each current in heap.trees()
-        if current.root < min then min = current
-    for each tree in min.subTrees()
-        tmp.addTree(tree)
-    heap.removeTree(min)
-    merge(heap, tmp)
+         min = heap.trees().first()
+         for each current in heap.trees()
+         if current.root < min then min = current
+         for each tree in min.subTrees()
+         tmp.addTree(tree)
+         heap.removeTree(min)
+         merge(heap, tmp)
          */
     }
 
     @Override
     public void Insert(int a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Node uusiNodejuuri = new Node(a);
+        Node[] uusiKeko = {uusiNodejuuri};
+        Merge(uusiKeko);
     }
 
     @Override
     public int getYlin() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 }
--- kekoilua/src/kekoilua/Node.java
+++ kekoilua/src/kekoilua/Node.java
@@ -1,18 +1,29 @@
 
 package kekoilua;
 
+import java.util.ArrayList;
+
 
 public class Node {
     
     private int value;
-    private Node next;
+    private ArrayList<Node> nexts;
     private Node prev;
-        
+    
+    public Node(int value){
+        this.value = value;
+        this.nexts = null;
+        this.prev = null;
+    }
+    public Node(){
+        this.nexts = null;
+        this.prev = null;
+    }
     public void setValue(int value){
         this.value = value;
     }
     public void setNext(Node next){
-        this.next = next;
+        nexts.add(next);
     }
     public void setPrev(Node prev){
         this.prev = prev;
@@ -21,10 +32,68 @@ public class Node {
     public int getValue(){
         return this.value;
     }
-    public Node getNext(){
-        return next;
+    public ArrayList<Node> getNext(){
+        return nexts;
     }
     public Node getPrev(){
         return prev;
     }
+    
+      private void Merge(Node toka) {
+         while(this.nexts != null && toka.nexts != null){
+             Node tree = MergeTree(this, toka);
+             if(this != null){
+                 tree = MergeTree(tree, this);
+             }
+         }
+        /*
+         * while not (p.end() and q.end())
+         tree = mergeTree(p.currentTree(), q.currentTree())
+        
+         if not heap.currentTree().empty()
+         tree = mergeTree(tree, heap.currentTree())
+        
+         heap.addTree(tree)
+         heap.next(); p.next(); q.next()
+         * 
+         * 
+         */
+        
+    }
+    
+    private Node addSubTree(Node eka, Node toka){
+        eka.setNext(toka);
+        return eka;
+    }
+
+    private Node MergeTree(Node eka, Node toka) {
+        if(eka.getValue() <= toka.getValue()){
+            return addSubTree(eka, toka);
+        }else{
+            return addSubTree(toka, eka);
+        }
+    }
+
+    public void Delete() {
+        
+        /*
+         * function deleteMin(heap)
+         min = heap.trees().first()
+         for each current in heap.trees()
+         if current.root < min then min = current
+         for each tree in min.subTrees()
+         tmp.addTree(tree)
+         heap.removeTree(min)
+         merge(heap, tmp)
+         */
+    }
+
+
+    public void Insert(int a) {
+        Node uusiNodejuuri = new Node(a);
+        Node[] uusiKeko = {uusiNodejuuri};
+        Merge(uusiKeko);
+    }
+    
+    
 }
