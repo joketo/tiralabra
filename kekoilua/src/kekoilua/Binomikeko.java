31e4bbbc15737bdfb6d293b2147db03777558aec
Johanna <johanna.keto@helsinki.fi>
2013-11-15 18:19:47 +0200

    binomikekoa eteenpäin

--- kekoilua/src/kekoilua/Binomikeko.java
+++ kekoilua/src/kekoilua/Binomikeko.java
@@ -2,10 +2,32 @@ package kekoilua;
 
 
 public class Binomikeko implements Keko{
-
+    
+    private void merge(int[]eka, int[]toka){
+        /*
+         * while not (p.end() and q.end())
+        tree = mergeTree(p.currentTree(), q.currentTree())
+        
+        if not heap.currentTree().empty()
+            tree = mergeTree(tree, heap.currentTree())
+        
+        heap.addTree(tree)
+        heap.next(); p.next(); q.next()
+         */
+    }
+    
     @Override
     public void Delete() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        /*
+         * function deleteMin(heap)
+    min = heap.trees().first()
+    for each current in heap.trees()
+        if current.root < min then min = current
+    for each tree in min.subTrees()
+        tmp.addTree(tree)
+    heap.removeTree(min)
+    merge(heap, tmp)
+         */
     }
 
     @Override
