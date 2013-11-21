b0f7cc96d95e9a01ea92a7cc641597a26443aea0
Johanna <johanna.keto@helsinki.fi>
2013-11-21 17:46:24 +0200

    lista kopioituu(ja koko tuplaantuu) järkevämmin

--- kekoilua/src/kekoilua/KasvavaArray.java
+++ kekoilua/src/kekoilua/KasvavaArray.java
@@ -1,9 +1,13 @@
 package kekoilua;
 
 import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
-public class KasvavaArray<T> {
+public class KasvavaArray<T> implements List {
 
     int viimeinen;
     T[] lista;
@@ -16,23 +20,9 @@ public class KasvavaArray<T> {
         lista = a;
     }
 
-    public void add(T alkio) {
-        if (viimeinen == lista.length - 1) {
-            @SuppressWarnings("unchecked")
-            T[] kopio = (T[]) Array.newInstance(cc, lista.length + 1);
-            kopio = lista.clone();
-            kopio[kopio.length] = alkio;
-            this.lista = kopio;
-        } else {
-            lista[viimeinen + 1] = alkio;
-            viimeinen = viimeinen + 1;
-        }
-
-    }
-
     public void delete(T alkio) {
         @SuppressWarnings("unchecked")
-        T[] kopio = (T[]) Array.newInstance(cc, lista.length - 1);
+        T[] kopio = Arrays.copyOf(lista, lista.length * 2);
         for (int j = 0; j <= lista.length; j++) {
             for (int i = 0; i < lista.length; i++) {
                 if (lista[i] == alkio) {
@@ -52,5 +42,129 @@ public class KasvavaArray<T> {
             }
         }
         return pituus;
+    }
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object[] toArray() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object[] toArray(Object[] ts) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean add(Object o) {
+        if (viimeinen == lista.length - 1) {
+            @SuppressWarnings("unchecked")
+            T[] kopio = Arrays.copyOf(lista, lista.length * 2);
+            kopio[kopio.length] = (T) o;
+            this.lista = kopio;
+        } else {
+            lista[viimeinen + 1] = (T) o;
+            viimeinen = viimeinen + 1;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean addAll(int i, Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean removeAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean retainAll(Collection clctn) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object get(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object set(int i, Object e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void add(int i, Object e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object remove(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListIterator listIterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ListIterator listIterator(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List subList(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
