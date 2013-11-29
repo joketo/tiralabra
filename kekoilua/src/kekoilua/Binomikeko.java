3ee418c6c13b66b2671cc5a155321a77afefda96
Johanna <johanna.keto@helsinki.fi>
2013-11-29 20:53:59 +0200

    binomikeon refaktorointia

--- kekoilua/src/kekoilua/Binomikeko.java
+++ kekoilua/src/kekoilua/Binomikeko.java
@@ -14,6 +14,19 @@ public class Binomikeko implements Keko {
         nykyinenPuu = rootList.getFirst();
     }
 
+    private void seuraava() {
+        nykyinenPuu = annaJuuriSisar(nykyinenPuu);
+    }
+
+    private TreeNode annaJuuriSisar(TreeNode node) {
+        int nykIndeksi = rootList.indexOf(node);
+        return rootList.get(nykIndeksi + 1);
+    }
+
+    private void lisaaPuu(TreeNode tree) {
+        rootList.add(tree);
+    }
+
     private void yhdistaKeot(Binomikeko a, Binomikeko b) {
         while (a != null && b != null) {
             TreeNode tree = a.nykyinenPuu.Yhdista(b.nykyinenPuu);
@@ -25,14 +38,6 @@ public class Binomikeko implements Keko {
             a.seuraava();
             b.seuraava();
         }
-    }
-
-    private void seuraava() {
-        nykyinenPuu = annaJuuriSisar(nykyinenPuu);
-    }
-
-    private void lisaaPuu(TreeNode tree) {
-        rootList.add(tree);
     }
 
     @Override
@@ -54,41 +59,39 @@ public class Binomikeko implements Keko {
     private void poistaPuu(TreeNode poistettava) {
         pienennaArvo(poistettava, Integer.MIN_VALUE);
         TreeNode min = rootList.getFirst();
-        for (TreeNode i:rootList){
-            if(i.getArvo() < min.getArvo()){
+        for (TreeNode i : rootList) {
+            if (i.getArvo() < min.getArvo()) {
                 min = i;
             }
         }
         rootList.remove(min);
         Binomikeko tmp = new Binomikeko();
-        List<TreeNode> list = min.annaLapset(); 
+        List<TreeNode> list = min.annaLapset();
         Collections.reverse(list); //reverse the order
-        for(TreeNode i : list){
+        for (TreeNode i : list) {
             tmp.lisaaPuu(i); //meneehän tää nyt oikeessa järjestyksessä...
         }
         this.yhdistaKeot(this, tmp);
     }
-    
-    private void pienennaArvo(TreeNode x, int uusiArvo){
-        if(uusiArvo > x.getArvo()){
+
+    private void pienennaArvo(TreeNode x, int uusiArvo) {
+        if (uusiArvo > x.getArvo()) {
             System.out.println("Uuden arvon tulee olla pienempi kuin nykyisen arvon");
             return;
         }
         x.setArvo(uusiArvo);
         TreeNode y = x;
         TreeNode z = y.getVanhempi();
-        while(z != null && y.getArvo() < z.getArvo()){
-            
+        while (z != null && y.getArvo() < z.getArvo()) {
+
             int yynArvo = y.getArvo(); //vaihdetaan arvot päittäin
             y.setArvo(z.getArvo());
             z.setArvo(yynArvo);
-            
+
             y = z;
             z = y.getVanhempi();
         }
     }
-    
-    
 
     @Override
     public void Insert(int a) {
@@ -96,11 +99,6 @@ public class Binomikeko implements Keko {
         TreeNode keonEkapuu = new TreeNode(a, 0); //onhan aste 0?
         uusikeko.lisaaPuu(keonEkapuu);
         yhdistaKeot(this, uusikeko);
-    }
-
-    private TreeNode annaJuuriSisar(TreeNode node) {
-        int nykIndeksi = rootList.indexOf(node);
-        return rootList.get(nykIndeksi + 1); 
     }
 
     @Override
